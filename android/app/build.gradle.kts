09480584a034f2713bf4c80fb7fbddc482afd1d1
ma611444400-collab <ma611444400@gmail.com>
2026-07-09 10:22:29 +0300

    Enable core library desugaring in build.gradle.kts
    Enable core library desugaring and add dependency.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,10 +12,11 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html)
         applicationId = "com.example.sportlivetv"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
@@ -42,4 +43,8 @@ kotlin {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
